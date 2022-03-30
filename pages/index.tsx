d9039aa80fee8cfb726e13b37031269172344d0d
DucDung_Tong <56964889+ducdung-tong@users.noreply.github.com>
2022-03-30 14:25:12 +0700

    Config some packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import Header from "../components/Header"
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+const Footer: NextPage = () => {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  )
 }
 
-export default Home
+export default Footer
