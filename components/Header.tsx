00e0c4079a5c0e62b4bb96e5d0b0ecad4bb8f563
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2022-05-16 23:02:54 +0700

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -355,12 +355,13 @@ const Header = () => {
                       favorite_border
                     </span>
                   </a>
-                  <a
-                    href="/post/upload-post"
-                    className="border-2 rounded-md border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200"
-                  >
-                    Đăng tin
-                  </a>
+                  <Link href="/post/upload-post">
+                    <a
+                      className="border-2 rounded-md border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200"
+                    >
+                      Đăng tin
+                    </a>
+                  </Link>
                   <span
                     className="h-6 w-px bg-gray-200"
                     aria-hidden="true"
