e09810b9ab89b80e99675f43c6e99f20c10713eb
truonglam66 <truongg166@gmail.com>
2022-05-27 10:40:41 +0700

    save change file interfaces/News.ts

--- pages/tin-tuc/thi-truong.tsx
+++ pages/tin-tuc/thi-truong.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import useSWR from 'swr'
-import { News } from '../../interfaces/News'
+import News from '../../interfaces/news'
 
 type Props = {
     newss: News[],
