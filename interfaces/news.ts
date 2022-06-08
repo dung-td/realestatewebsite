5df6999d65f6fbc84455a5bfe9eb71b2a0c34160
DucDung_Tong <tngcdng@gmail.com>
2022-06-08 19:58:28 +0700

    update

---
+++ interfaces/news.ts
@@ -0,0 +1,18 @@
+export default interface News {
+    _id: string
+    title: string
+    body: [NewsDetails]
+    slug: string
+    author: string
+    description: string
+    submitday: string
+    tags: string
+    type: string
+    views: number
+    thumbnail: string
+  }
+  
+  interface NewsDetails {
+    type: string
+    src: string
+  }
